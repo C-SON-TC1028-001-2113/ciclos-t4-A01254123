7003db750ff8eff573c32168fd06deadbaaf94e6
A01254123 <A01254123@itesm.mx>
2021-09-06 02:04:59 +0000

    Triangulos asteriscos_CesarMtz

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,7 +1,7 @@
-
 def main():
     #escribe tu código abajo de esta línea
-
-
+    altura=int(input("Enter triangle height: "))
+    for i in range(1,altura+1):
+        print((altura-i)*" "+"*"*i)
 if __name__=='__main__':
     main()
