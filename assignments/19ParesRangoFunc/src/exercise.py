b99478a2bd285f507e5d629f9f72e15edf5b66dd
A01254123 <A01254123@itesm.mx>
2021-09-06 00:03:52 +0000

    Ejercicio19_CesarMtz

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,6 +1,17 @@
-
 def main():
-
-
+    numero1=int(input("Valor 1: "))
+    numero2=int(input("Valor 2: "))
+    if numero1>0 and numero2>0:
+        if numero1!=numero2:   
+            if numero1>numero2:
+                for i in range(numero2,numero1+1):
+                    if i%2==0:
+                        print(i)
+            elif numero1<numero2:
+                for i in range(numero1,numero2+1):
+                    if i%2==0:
+                        print(i)
+        else:
+            print("No hay pares")
 if __name__=='__main__':
     main()
